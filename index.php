6dee3f7074586b9d96cec4c8ae48f79923ddd87f
Ryan Cwynar <ryan@fueledonbacon.com>
2020-08-16 18:01:58 -0400

    simplify the php

--- index.php
+++ index.php
@@ -24,58 +24,12 @@ function tailwind_load_textdomain()
   load_plugin_textdomain('tailwind', false, basename(__DIR__) . '/languages');
 }
 
-/** 
- * Add custom image size for block featured image.
- * 
- * @link https://developer.wordpress.org/reference/functions/add_image_size/
- */
-add_action('init', 'tailwind_add_image_size');
-
-function tailwind_add_image_size()
-{
-  add_image_size('tailwindFeatImg', 250, 250, array('center', 'center'));
-}
-
-/** 
- * Register custom image size with sizes list to make it available.
- * 
- * @link https://codex.wordpress.org/Plugin_API/Filter_Reference/image_size_names_choose
- */
-add_filter('image_size_names_choose', 'tailwind_custom_sizes');
-
-function tailwind_custom_sizes($sizes)
-{
-  return array_merge($sizes, array(
-    'tailwindFeatImg' => __('Tailwind Featured Image'),
-  ));
-}
-
 /**
  * Makes Tailwind available in the block editor
  *
  * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-registration/
  */
-add_action('init', 'tailwind_register_blocks');
-add_action('enqueue_block_editor_assets', 'tailwind_register_blocks');
 
-function tailwind_register_blocks()
-{
-
-  // If Block Editor is not active, bail.
-  if (!function_exists('register_block_type')) {
-    return;
-  }
-
-  // Register the front-end stylesheet.
-  wp_register_style(
-    'tailwind-style',                    // label
-    plugins_url('dist/tailwind.css', __FILE__),            // CSS file
-    array(),    // dependencies
-    filemtime(plugin_dir_path(__FILE__) . 'dist/tailwind.css')  // set version as file last modified time
-  );
-}
-
-// Custom PHP Below
 
 function tailwind_plugin_scripts()
 {
@@ -87,5 +41,6 @@ function tailwind_plugin_scripts()
 }
 
 add_action('wp_enqueue_scripts', 'tailwind_plugin_scripts');
+add_action('admin_enqueue_scripts', 'tailwind_plugin_scripts');
 
 
